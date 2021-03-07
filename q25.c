b8022ae037414367815ed7824f501618670aadbe
Thuan Ho <tho03@g.uafs.edu>
2021-03-07 17:44:15 +0000

    Finished

---
+++ q25.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+   char ch1 = '1';
+   char ch2 = '8';
+   char ch3;
+
+   ch3 = ch1 + ch2;
+   printf("ch1: %c - %d\n",ch1,ch1);
+   printf("ch2: %c - %d\n",ch2,ch2);
+   printf("ch3: %c - %d\n",ch3,ch3);
+   return 0;
+}
